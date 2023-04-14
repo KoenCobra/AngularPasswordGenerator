48d1c263d81e5a59b06418907496f8cc02727ff8
Koen De Groot <koen.de.groot@organimmo.be>
2023-04-14 15:07:59 +0200

    Added generate password logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'password';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  length = 0;
+  password = '';
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols
+  }
+
+  onChangeLength(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue
+    }
+  }
+
+  onButtonClick() {
+    const numbers = '123456789';
+    const letters = 'azertyuiopqsdfghjklmwxcvbn';
+    const symbols = '/.;,%£^$&é@#"(§è!çà)}{';
+
+    let validChars = '';
+
+    if (this.includeLetters) {
+      validChars += letters
+    }
+    if (this.includeNumbers) {
+      validChars += numbers
+    }
+    if (this.includeSymbols) {
+      validChars += symbols
+    }
+
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
 }
